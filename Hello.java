c3a32189aa00d579ce062f98579fcd8fda823b25
Sam Luther <samluther998@gmail.com>
2024-09-30 10:06:21 -0600

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,5 +3,6 @@ package week6;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, this is Selah. Testing, testing.");
+        System.out.println("Hello, this is Sam. Looks good!");
     }
 }
